3c9823812701d9bb18462ac427ff52080a1240c4
dan <dan.bate@gmail.com>
2014-05-05 23:15:12 +0100

    added get_authenticated _users

---
+++ django_cookbook/users.py
@@ -0,0 +1,35 @@
+import datetime
+from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
+
+
+def get_authenticated_users(include=[], exclude=[]):
+    """
+    Gets a lit of all the authenticated user who are in the include list and not in the exclude list
+
+    :param include:
+    :param exclude:
+    :return:
+    """
+    # get the ids of the authenticated sessions
+    sessions = Session.objects.filter(expire_date__gte=datetime.now())
+    uid_list = []
+    for session in sessions:
+        data = session.get_decoded()
+        uid_list.append(data.get('_auth_user_id', None))
+
+
+    #find all the users
+    if include:
+        return User.objects.filter(
+            id__in=uid_list,
+            id__in=[u.id for u in include],
+        ).exclude(
+            id__in=[u.id for u in exclude]
+        )
+    else:
+        return User.objects.filter(
+            id__in=uid_list,
+        ).exclude(
+            id__in=[u.id for u in exclude]
+        )
